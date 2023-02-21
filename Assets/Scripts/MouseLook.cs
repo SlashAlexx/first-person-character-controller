2d360607cf76e807704149c42e3238aeb40a0edb
SlashAlexx <119014375+SlashAlexx@users.noreply.github.com>
2023-02-20 18:40:17 -0600

    Basic Controller and Environment
    Added Basic character controller with looking, movement, and jump functionality. Also added basic setup for testing environment with prototype textures.

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+    public float look_sensitivity = 100f;
+    public Transform player_body;
+    float x_rotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouse_position_x = Input.GetAxis("Mouse X") * look_sensitivity * Time.deltaTime;
+        float mouse_position_y = Input.GetAxis("Mouse Y") * look_sensitivity * Time.deltaTime;
+
+        x_rotation -= mouse_position_y;
+        x_rotation = Mathf.Clamp(x_rotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(x_rotation, 0, 0);
+        player_body.Rotate(Vector3.up * mouse_position_x);
+    }
+}
